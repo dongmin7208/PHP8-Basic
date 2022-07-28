d1f7b259abba0b69bca892b8a7290124b3449620
dongmin7208 <sayg3o3d@gmail.com>
2022-07-28 11:43:28 +0900

    constant

---
+++ php1/ex3_7.php
@@ -0,0 +1,7 @@
+<?php
+  define("FAVORITE_FRUIT","apple");
+  echo "제일 좋아하는 과일은 ".FAVORITE_FRUIT."입니다";
+  
+  // define("FAVORITE_FRUIT","banana");
+  //echo "제일 좋아하는 과일은 ".FAVORITE_FRUIT."입니다";
+?>
