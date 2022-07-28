9713994414dcc44d731c14fc89e0ac0fa815840c
dongmin7208 <sayg3o3d@gmail.com>
2022-07-28 16:12:18 +0900

    for(if if)

--- php1/ex8_4.php
+++ php1/ex8_4.php
@@ -1,8 +1,12 @@
 <?php
     $sum=0;
+    $cnt=0;
     for($i=1; $i<=20; $i++){
         if($i%2 ==0){
-            $sum=$sum+$i;
+            $sum+=$i;
+            // $cnt+=1; // $cnt = $cnt +1;
+            $cnt++;
         }
     }
-    echo "1~10 sum : ".$sum;echo"<br>";
+    echo "1~20 sum : ".$sum;echo"<br>";
+    echo "1~20 sum 갯수 : ".$cnt;
