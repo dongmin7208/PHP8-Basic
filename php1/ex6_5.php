bee0bf81e210ee71c6773b5cb68ae62d3f8b889e
dongmin7208 <sayg3o3d@gmail.com>
2022-07-28 14:41:43 +0900

    elseif elseif else

---
+++ php1/ex6_5.php
@@ -0,0 +1,18 @@
+<?php
+    $num=1;
+
+    if($num == 1){
+        echo"east";
+    }
+    elseif($num == 2){
+        echo"west";
+    }
+    elseif($num == 3){
+        echo"south";
+    }
+    elseif($num == 4){
+        echo"north";
+    }else{
+        echo "wrong";
+    }
+    ?>
