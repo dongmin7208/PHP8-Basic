240a1778709aaea8d210d13a694b61d5a0ecdf37
dongmin7208 <sayg3o3d@gmail.com>
2022-07-28 16:34:48 +0900

    for(for) - 123 456 789

---
+++ php1/ex9_1.php
@@ -0,0 +1,11 @@
+<?php
+    $cnt=0;
+
+    for($i=1; $i<=3; $i++){
+        for($j=1; $j<=3; $j++){
+            $cnt++;
+            echo "&nbsp;&nbsp;".$cnt;
+        }
+        echo"<br>";
+    }
+?>
