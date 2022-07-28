99cba0bbab65cf2007e5b2353782fac1fe9129f6
dongmin7208 <sayg3o3d@gmail.com>
2022-07-28 16:21:57 +0900

    for(if+if)

--- php1/ex8_5.php
+++ php1/ex8_5.php
@@ -10,9 +10,13 @@
             $sum7+=$i;
         }
     }
+    
+        echo $sum7;echo"<br>";
+        echo $sum3;echo"<br>";
 
     if($sum3>$sum7){
-        echo "3의 배수의 합계가 더 큽니다: ".$sum3;
+        echo "3의 배수의 합계가 더 큽니다: ".$sum3;echo"<br>";
     }else if ($sum3<$sum7){
-        echo "7의 배수의 합계가 더 큽니다.".$sum7;
+        echo "7의 배수의 합계가 더 큽니다.".$sum7;echo"<br>";
     }
+?>
