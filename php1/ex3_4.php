6dc0aa5f9e09b9a8d5a3c6d2b855fedf2669f578
dongmin7208 <sayg3o3d@gmail.com>
2022-07-28 11:17:11 +0900

    配列[ [ [] ] ] 一緒

---
+++ php1/ex3_4.php
@@ -0,0 +1,11 @@
+<?php
+    $arr=array(
+        array("apple","japan",1000),
+        array("banana","korea",2000),
+        array("orange","us",1500)
+    );
+
+echo $arr[0][0]," ", $arr[0][1]," ", $arr[0][2], "<br>";
+echo $arr[1][0]," ", $arr[1][1]," ", $arr[1][2], "<br>";
+echo $arr[2][0]," ", $arr[2][1]," ", $arr[2][2];
+?>
