9be1c6774c8c17b3f932fa13b74aee9cf11462a3
dongmin7208 <sayg3o3d@gmail.com>
2022-07-28 14:34:41 +0900

    if elseif

---
+++ php1/ex6_3.php
@@ -0,0 +1,17 @@
+<?php
+    $score=60;
+    if($score >=90){
+        echo"A score";
+    }
+    elseif($score >=80){
+        echo"B score";
+    }
+    elseif($score >=70){
+        echo"C score";
+    }
+    elseif($score >=60){
+        echo"D score";
+    }else{
+        echo "F scroe";
+    }
+    ?>
