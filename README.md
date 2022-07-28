43727cad1f38e250de4ad579390b9b7053de5c6d
dongmin7208 <sayg3o3d@gmail.com>
2022-07-28 12:47:56 +0900

    README.md

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # PHP basic!
+
+## document Link - notion
+
+## https://seemly-gate-a3d.notion.site/PHP-Hypertext-Preprocessor-9b844618e2674b9ebd0ec4214e5cdd5d
--- php1/ex3_2.php
+++ php1/ex3_2.php
@@ -3,6 +3,7 @@
     $arr[0] = "apple";
     $arr[1] = "banana";
     $arr[2] = "orange";
+    $arr[3] = "orange2";
 
     echo $arr[0];
     echo "<br>";
@@ -10,5 +11,8 @@
     echo "<br>";
     echo $arr[2];
     echo "<br>";
+    echo $arr[3];
+    
+
 
 ?>
