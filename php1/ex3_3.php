2a328a3d91a235530bd672c05e7494498b11448b
dongmin7208 <sayg3o3d@gmail.com>
2022-07-28 11:11:31 +0900

    配列[ [ [] ] ]

---
+++ php1/ex3_3.php
@@ -0,0 +1,22 @@
+<?php
+    $arr=array(
+        array(),
+        array(),
+        array()
+    );
+$arr[0][0] = "apple";
+$arr[0][1] = "japan";
+$arr[0][2] = 1000;
+
+$arr[1][0]="banana";
+$arr[1][1]="korea";
+$arr[1][2]=2000;
+
+$arr[2][0]="orange";
+$arr[2][1]="us";
+$arr[2][2]=1500;
+
+echo $arr[0][0]," ", $arr[0][1]," ", $arr[0][2], "<br>";
+echo $arr[1][0]," ", $arr[1][1]," ", $arr[1][2], "<br>";
+echo $arr[2][0]," ", $arr[2][1]," ", $arr[2][2];
+?>
