0484b863c1c03f81f57979d4ad2891b73eb65553
dongmin7208 <sayg3o3d@gmail.com>
2022-07-28 11:04:53 +0900

    配列[ ]

---
+++ php1/ex3_2.php
@@ -0,0 +1,14 @@
+<?php
+    $arr = array();
+    $arr[0] = "apple";
+    $arr[1] = "banana";
+    $arr[2] = "orange";
+
+    echo $arr[0];
+    echo "<br>";
+    echo $arr[1];
+    echo "<br>";
+    echo $arr[2];
+    echo "<br>";
+
+?>
