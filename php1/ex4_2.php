a867969984691920f7a90f773e6dd3b50310e6e4
dongmin7208 <sayg3o3d@gmail.com>
2022-07-28 12:13:03 +0900

    type bool

---
+++ php1/ex4_2.php
@@ -0,0 +1,17 @@
+<?php
+    $res = 20;
+
+    $res += 10;
+    echo $res."<br>";
+
+    $res -= 10;
+    echo $res."<br>";
+
+    $res *= 10;
+    echo $res."<br>";
+
+    $res /= 10;
+    echo $res."<br>";
+
+    $res %= 10;
+    echo $res."<br>";
---
+++ php1/ex4_3.php
@@ -0,0 +1,32 @@
+<?php
+    $a = 10; $b=5;
+    echo "a=".$a.", b=".$b;
+    echo "<br>";
+
+    $c = $a < $b;
+    var_dump($c);
+    echo"<br>";
+    
+    $d = $a > $b;
+    var_dump($c);
+    echo"<br>";
+    
+    $c = $a <= $b;
+    var_dump($c);
+    echo"<br>";
+    
+    $c = $a >= $b;
+    var_dump($c);
+    echo"<br>";
+    
+    $c = $a == $b;
+    var_dump($c);
+    echo"<br>";
+
+    $c = $a === $b;
+    var_dump($c);
+    echo"<br>";
+
+    $c = $a != $b;
+    var_dump($c);
+    echo"<br>";
