9b1824ab007d1a7be1f2021f7c4feb128d8572ba
dongmin7208 <sayg3o3d@gmail.com>
2022-07-28 11:56:33 +0900

    + - % *

---
+++ php1/ex4_1.php
@@ -0,0 +1,18 @@
+<?php
+    $res = 20+10;
+    echo $res."<br>";
+
+    $res = 20-10;
+    echo $res."<br>";
+
+    $res= 20*10;
+    echo $res."<br>";
+
+    $res= 20/10;
+    echo $res."<br>";
+
+    $res= 20%10;
+    echo $res."<br>";
+
+    $res = 2**3;
+    echo $res."<br>";
