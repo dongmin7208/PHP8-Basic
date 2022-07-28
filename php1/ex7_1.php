76db8f6f9f05b57adbc82de6cba855297ca8749b
dongmin7208 <sayg3o3d@gmail.com>
2022-07-28 14:56:01 +0900

    switch

---
+++ php1/ex7_1.php
@@ -0,0 +1,17 @@
+<?php
+    $num=3;
+
+    switch($num){
+        case 1:
+            echo '예금조회'; break;
+        case 2:
+            echo '출금'; break;
+        case 3:
+            echo '입금'; break;
+        case 4:
+            echo '이체'; break;
+        default:
+            echo '잘못 누르셨습니다.'; 
+
+    }
+    ?>
