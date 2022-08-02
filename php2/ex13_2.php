4de8740338e3fda055a1bccf002f2505a436b51f
dongmin7208 <sayg3o3d@gmail.com>
2022-08-02 12:33:48 +0900

    array

---
+++ php2/ex13_2.php
@@ -0,0 +1,11 @@
+<?php
+
+    $array=array();
+
+    $array["one"] = "apple";
+    $array["two"] = "orange";
+    $array["three"] = "banana";
+
+    echo $array["one"]."<br>";
+    echo $array["two"]."<br>";
+    echo $array["three"]."<br>";
