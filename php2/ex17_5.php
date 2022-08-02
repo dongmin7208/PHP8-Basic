e87105a81a34dd8744ab115789e630d7cd3b8261
dongmin7208 <sayg3o3d@gmail.com>
2022-08-02 17:32:08 +0900

    strlen + substr

---
+++ php2/ex17_5.php
@@ -0,0 +1,6 @@
+<?php
+$str = "beautiful";
+$str2 = "ありがとう";
+
+echo $str."の　数 : ".strlen($str)."<br>";
+echo $str2."の　数 : ".strlen($str2)."<br>";
---
+++ php2/ex18_1.php
@@ -0,0 +1,12 @@
+<?php
+$str="2022-10-22 21:30:50";
+$year = substr($str,0,4);
+$month = substr($str,5,2);
+$day = substr($str,8,2);
+$time =substr($str,11);
+
+echo $year."년 <br>";
+echo $month."월 <br>";
+echo $day."일 <br>";
+echo $time;
+?>
