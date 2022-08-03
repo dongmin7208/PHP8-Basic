d67cfa3e1515e401f569eef594cd2934003f322d
dongmin7208 <sayg3o3d@gmail.com>
2022-08-03 10:05:58 +0900

    array_pop

---
+++ php2/ex19_4.php
@@ -0,0 +1,18 @@
+<?php
+$fruit = array("さる","猿２","猿３","猿４","猿5","猿6","猿7");
+print_r($fruit);
+echo"<br>";
+
+array_pop($fruit);
+print_r($fruit);
+echo "<br>";
+
+array_pop($fruit);
+print_r($fruit);
+echo "<br>";
+
+array_pop($fruit);
+print_r($fruit);
+echo "<br>";
+
+?>
