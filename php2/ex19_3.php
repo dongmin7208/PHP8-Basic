a1ca240513d85bb14270addaa4a0eeea9eeb4849
dongmin7208 <sayg3o3d@gmail.com>
2022-08-03 09:58:12 +0900

    array_push + print_r

---
+++ php2/ex19_3.php
@@ -0,0 +1,11 @@
+<?php
+$fruit = array("さる","猿２","猿３","猿４");
+print_r($fruit);
+echo"<br>";
+
+array_push($fruit, "tomato");
+array_push($fruit, "tomato2");
+array_push($fruit, "tomato3");
+print_r($fruit);
+
+?>
