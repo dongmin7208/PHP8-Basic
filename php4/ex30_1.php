49b38c6aa2781fe8010cf2129c0962df4fd1d259
dongmin7208 <sayg3o3d@gmail.com>
2022-08-04 13:34:09 +0900

    isset()

---
+++ php4/ex30_1.php
@@ -0,0 +1,18 @@
+<?php
+    if(isset($str)){
+        echo "変数strが存在します。";
+    }else{
+        echo "変数strが存在しません。";
+
+    }
+    echo "<br>";
+
+    $str ="string";
+    if(isset($str)){
+        echo "変数strが存在します。";
+    }
+    else{
+        echo "変数strが存在しません。";
+
+    }
+    
