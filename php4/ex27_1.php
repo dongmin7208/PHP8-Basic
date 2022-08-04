2a4d2947f373f3f9e2cf11976b5b3e1b5039b678
dongmin7208 <sayg3o3d@gmail.com>
2022-08-04 09:30:23 +0900

    공백 없애기

---
+++ php4/ex27_1.php
@@ -0,0 +1,12 @@
+<?php
+    $str="      両方모두공백 없애기     ";
+    echo "|".trim($str)."|";
+    echo "<br>";
+    
+    $str="      前 공백 없애기      ";
+    echo "|".ltrim($str)."|";
+    echo "<br>";
+  
+    $str="      裏 공백 없애기     ";
+    echo "|".rtrim($str)."|";
+    echo "<br>";
