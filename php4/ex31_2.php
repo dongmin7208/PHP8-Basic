5c5f18c8e50b93f6a7b1874a1e9a23f0464efa1a
dongmin7208 <sayg3o3d@gmail.com>
2022-08-04 15:00:39 +0900

    []

--- php4/ex31_2.php
+++ php4/ex31_2.php
@@ -7,3 +7,7 @@
     echo $fruit[2];
     echo "<br>";
     echo $fruit[4];
+
+    for($i=0; $i<count($fruit); $i++){
+        echo $fruit[$i]."<br>";
+    }
---
+++ php4/ex31_3.php
@@ -0,0 +1,12 @@
+<?php
+    $fruit = array();
+    $fruit = ['banana','water melon', 'grape', 'apple', 'mango'];
+
+
+
+    list($first, $second, $third)=$fruit;
+    
+    
+    echo $first."<br>";
+    echo $second."<br>";
+    echo $third."<br>";
